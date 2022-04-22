91b4f85b1c839964d54fcb6f310d206be7b97ac7
RafinurM <93291025+RafinurM@users.noreply.github.com>
2022-04-22 20:25:19 +0700

    updated

--- app.js
+++ app.js
@@ -74,15 +74,3 @@ function colorCoding() {
     }
 }
 
-const inputs = document.querySelectorAll('input[type=number]');
-Array.from(inputs).forEach(input => {
-    const min = +input.min;
-    const max = +input.max;
-
-    input.addEventListener('input', (e) => {
-        const value = +input.value;
-        if (value > max) { input.value = max }
-        else if (value < min) { input.value = min }
-    })
-
-});
